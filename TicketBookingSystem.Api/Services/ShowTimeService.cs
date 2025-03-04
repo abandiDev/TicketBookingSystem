79cb4adc9a31b40c6ff72e203912aa3376e6087b
unknown <abhilashbandi93@gmail.com>
2025-03-04 19:52:44 +0530

    Get Peak booking hours

--- TicketBookingSystem.Api/Services/ShowTimeService.cs
+++ TicketBookingSystem.Api/Services/ShowTimeService.cs
@@ -9,29 +9,29 @@ public class ShowTimeService : IShowTimeService
 {
     private readonly TicketBookingDbContext _dbContext;
 
-    public ShowTimeService(TicketBookingDbContext  context)
+    public ShowTimeService(TicketBookingDbContext context)
     {
         _dbContext = context;
     }
-    
+
     public async Task<Result<Reservation>> Book(int showTimeId, BookShowDto dto)
     {
         await using var transaction = await _dbContext.Database.BeginTransactionAsync();
         var showTime = await _dbContext.Showtimes
             .Include(x => x.Reservations)
             .FirstOrDefaultAsync(x => x.Id == showTimeId);
-        
+
         if (showTime == null)
         {
             return Result<Reservation>.Failure("Invalid show time");
         }
-        
-        var result = showTime.Book(dto.UserId , dto.NumberOfSeats);
+
+        var result = showTime.Book(dto.UserId, dto.NumberOfSeats);
         if (result.IsFailure)
         {
             return Result<Reservation>.Failure(result.Error);
         }
-        
+
         await _dbContext.SaveChangesAsync();
         await transaction.CommitAsync();
 
@@ -48,7 +48,7 @@ public class ShowTimeService : IShowTimeService
         {
             return Result<Reservation>.Failure("Invalid show time");
         }
-        
+
         var result = showTime.Cancel(reservationId);
         await _dbContext.SaveChangesAsync();
         await transaction.CommitAsync();
@@ -57,7 +57,8 @@ public class ShowTimeService : IShowTimeService
 
     public async Task<Result<Reservation>> GetReservation(int showTimeId, int reservationId)
     {
-        var reservation = await _dbContext.Reservations.Include(x => x.Showtime).FirstOrDefaultAsync(x => x.Id == reservationId && x.ShowtimeId == showTimeId);
+        var reservation = await _dbContext.Reservations.Include(x => x.Showtime)
+            .FirstOrDefaultAsync(x => x.Id == reservationId && x.ShowtimeId == showTimeId);
         if (reservation == null)
         {
             return Result<Reservation>.Failure("Invalid reservation");
@@ -71,14 +72,21 @@ public class ShowTimeService : IShowTimeService
         return await _dbContext.Reservations
             .Include(x => x.Showtime)
             .ThenInclude(x => x.Movie)
-            .Where( x => x.UserId == userId )
+            .Where(x => x.UserId == userId)
             .ToListAsync();
     }
 
     public async Task<List<PopularShowtimesDto>> GetPopularShows(int topN)
     {
         var popularShowtimes = await _dbContext.Set<PopularShowtimesDto>()
-            .FromSqlInterpolated($"select\n(s.\"SeatingCapacity\"  - s.\"AvailableSeats\") as BookedSeats, m.\"Title\", s.\"StartTime\"\nfrom \"Showtimes\" s \njoin \"Movies\" m \non m.\"Id\" = s.\"MovieId\"\norder by 1 desc\nlimit {topN}")
+            .FromSqlInterpolated(
+                $@"select
+                    (s.""SeatingCapacity""  - s.""AvailableSeats"") as BookedSeats, m.""Title"", s.""StartTime""
+                    from ""Showtimes"" s 
+                    join ""Movies"" m 
+                    on m.""Id"" = s.""MovieId""
+                    order by 1 desc
+                    limit {topN}")
             .ToListAsync();
         return popularShowtimes;
     }
@@ -90,13 +98,13 @@ public class ShowTimeService : IShowTimeService
                 $@"SELECT 
                    date_part('hour', ""BookingTime"") AS BookingHour,
                     COUNT(*) AS ReservationCount,
-                    SUM(""NumberOfSeats"") AS TotalSeatsBooked
+                    SUM(CASE WHEN ""Status"" = 0 THEN ""NumberOfSeats"" ELSE 0 END) AS TotalSeatsBooked
                 FROM ""Reservations""
                 GROUP BY BookingHour
                 ORDER BY ReservationCount DESC
                 LIMIT {topN};")
             .ToListAsync();
-        
+
         return peakBookingHours;
     }
 }
